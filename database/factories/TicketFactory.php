9268b53b6a943b214c3bc46733f7795bd4a8c001
Quentin M <qmarchand@agencecube.fr>
2022-04-07 16:01:34 +0200

    initial commit

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'summary' => $this->faker->sentence(),
+            'description' => $this->faker->unique()->paragraph(),
+            'status' => $this->faker->word(),
+            
+        ];
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tickets/create', [TicketController::class,'create'])->name('tickets.create');
+
+Route::post('/tickets/create', [TicketController::class,'store'])->name('tickets.store');
+
+Route::get('/tickets/delete/{ticket}', [TicketController::class,'delete'])->name('tickets.delete');
+
+Route::post('/tickets/delete/{ticket}', [TicketController::class,'destroy'])->name('tickets.destroy');
+
+Route::get('/tickets/{ticket}', [TicketController::class,'show'])->name('tickets.show');
+
+Route::post('/tickets/{ticket}', [TicketController::class,'update'])->name('tickets.update');
+
+Route::get('/tickets', [TicketController::class,'index'])->name('tickets.index');
+
+
